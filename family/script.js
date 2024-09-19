c05b9dcbefbe7a67e3fba7b4705121f2bc91ab8e
sofiyachouhan <sofiyachouhan1312@gmail.com>
2024-09-19 20:25:44 +0530

    added index.html

---
+++ family/script.js
@@ -0,0 +1,15 @@
+document.getElementById("button1").addEventListener("click", function() {
+    window.open("page1.html", "_blank"); // Replace with the URL of Page 1
+});
+
+document.getElementById("button2").addEventListener("click", function() {
+    window.open("page2.html", "_blank"); // Replace with the URL of Page 2
+});
+
+document.getElementById("button3").addEventListener("click", function() {
+    window.open("page3.html", "_blank"); // Replace with the URL of Page 3
+});
+
+document.getElementById("button4").addEventListener("click", function() {
+    window.open("another_family_page.html", "_blank"); // Replace with the URL of another family page
+});
